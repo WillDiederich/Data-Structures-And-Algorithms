5bf5fe0c89a94e239f8e2ed3e5199dde9103c4b2
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-11 13:13:39 -0800

    ...

--- src/Lists/DoublyLinkedList.java
+++ src/Lists/DoublyLinkedList.java
@@ -1,14 +1,11 @@
 package Lists;
-
 import java.util.Collection;
-import java.util.LinkedList;
 
 public class DoublyLinkedList<E> {
-    class Node<E>{
-
+    private static class Node<E> {
         E data;
-        Node previous;
-        Node next;
+        Node<E> previous;
+        Node<E> next;
 
         Node(E e){
             this.data = e;
@@ -17,20 +14,20 @@ public class DoublyLinkedList<E> {
         }
     }
 
-    Node head = null;
-    Node tail = null;
+    Node<E>head = null;
+    Node<E> tail = null;
     int size;
 
-    public void DoublyLinkedList(){
-
+    public DoublyLinkedList() {
     }
 
-    public void add(E e){
-        Node newNode = new Node(e);
-        if(head == null){
+    // Adds a new node to the end of the list
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if(head == null) {
             head = newNode;
         }
-        else{
+        else {
             tail.next = newNode;
             newNode.previous = tail;
         }
@@ -38,41 +35,52 @@ public class DoublyLinkedList<E> {
         size++;
     }
 
-    public void add(Collection<? extends E> c){
-        for(E e : c){
+    // Adds a collection of values to the list
+    public void add(Collection<? extends E> c) {
+        for(E e : c) {
             add(e);
         }
     }
 
-    public void remove(E e){
-        if(head == null){
-            return;
-        }
-        else if(head.data == e && tail.data == e){
-            head = null;
-            tail = null;
-        }
-        else if(head.data == e){
-            head = head.next;
-            head.previous = null;
-        }
-        else if(tail.data == e){
-            tail = tail.previous;
-            tail.next = null;
-        }
-        else{
-            Node current = head;
-            while(current != null && current.data != e){
-                current = current.next;
+    // Remove first occurrence of a node, if it exists.
+    // This method is a mess and will be updated.
+    public void remove(E e) {
+        // List is not empty.
+        if(head != null) {
+            // List contains a single node and that node is our target node.
+            if (head.data == e && tail.data == e) {
+                head = null;
+                tail = null;
+                size--;
             }
-            if(current != null && current.data == e){
-                current.previous.next = current.next;
-                current.next.previous = current.previous;
+            // The head of the list is our target node.
+            else if (head.data == e) {
+                head = head.next;
+                head.previous = null;
+                size--;
+            }
+            // The tail of the list is our target node.
+            else if (tail.data == e) {
+                tail = tail.previous;
+                tail.next = null;
+                size--;
+            }
+            // The target node is potentially somewhere between the head of the list and the tail.
+            else {
+                Node<E> current = head;
+                while (current != null && current.data != e) {
+                    current = current.next;
+                }
+                if (current != null) {
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
+                    size--;
+                }
             }
         }
-        size--;
     }
 
+    // Returns true if the list is empty, false if the list contains one or mode nodes.
     public boolean isEmpty(){
         return head == null;
     }
