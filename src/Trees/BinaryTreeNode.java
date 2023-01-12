94fb8e4394518e04415a981d0a4d63d740c26d2b
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-12 13:30:11 -0800

    ...

--- src/Trees/BinaryTreeNode.java
+++ src/Trees/BinaryTreeNode.java
@@ -10,4 +10,17 @@ public class BinaryTreeNode {
         this.left = null;
         this.right = null;
     }
+
+    public BinaryTreeNode add(BinaryTreeNode root, int value){
+        if(root == null){
+            return new BinaryTreeNode(value);
+        }
+        else if(value < root.value){
+            root.left = add(root.left, value);
+        }
+        else{
+            root.right = add(root.right, value);
+        }
+        return root;
+    }
 }
