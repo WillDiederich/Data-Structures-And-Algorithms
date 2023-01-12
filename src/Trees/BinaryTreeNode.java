77781d8427b857ef30a2222c5b057beeb0a2c0eb
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-12 12:19:05 -0800

    ...

---
+++ src/Trees/BinaryTreeNode.java
@@ -0,0 +1,15 @@
+package Trees;
+
+public class BinaryTreeNode {
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
