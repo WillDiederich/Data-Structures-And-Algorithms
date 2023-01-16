25a7a740a75d5bb38cda5b13d91cd7b3e9f486c9
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-15 18:32:33 -0800

    ...

--- src/Graphs/TopologicalSort_DFS.java
+++ src/Graphs/TopologicalSort_DFS.java
@@ -5,59 +5,39 @@ import java.util.List;
 import java.util.Stack;
 
 public class TopologicalSort_DFS {
-    public static Stack<Integer> topologicalSort(int[][] graph, int v){
+    public static Stack<Integer> topologicalSort(int[][] graph, int v) {
         List<List<Integer>> adjacencyList = buildGraph(graph, v);
         boolean[] visited = new boolean[v];
         Stack<Integer> result = new Stack<>();
-        for(int currentVertex = 0; currentVertex < v; currentVertex++){
+        for(int currentVertex = 0; currentVertex < v; currentVertex++) {
             if(!visited[currentVertex]){
-                result = visit(adjacencyList, visited, result, currentVertex);
+                if(dfs(adjacencyList, visited, new boolean[v], currentVertex, result)) {
+                    System.out.println("Cycle detected.");
+                    return null;
+                }
             }
         }
         return result;
     }
 
-    public static Stack<Integer> visit(List<List<Integer>> adjacencyList, boolean[] visited, Stack<Integer> result, int currentVertex){
-        if(visited[currentVertex]){
-            return result;
+    // Returns true if a cycle has been detected
+    public static boolean dfs(List<List<Integer>> adjacencyList, boolean[] visited, boolean[] checkCycle, int currentVertex, Stack<Integer> result) {
+        if(visited[currentVertex]) {
+            return false;
         }
-        for(int nextVertex : adjacencyList.get(currentVertex)){
-            result = visit(adjacencyList, visited, result, nextVertex);
+        if(checkCycle[currentVertex]) {
+            return true;
         }
-        visited[currentVertex] = true;
-        result.push(currentVertex);
-        return result;
-    }
-
-    // Detect Cycles in a graph:
-    public static boolean topologicalSortDetectCycles(int[][] graph, int v){
-        List<List<Integer>> adjacencyList = buildGraph(graph, v);
-        boolean[] visited = new boolean[v];
-        for(int currentVertex = 0; currentVertex < v; currentVertex++){
-            if(!visited[currentVertex]){
-                if(!visit(adjacencyList, visited, new boolean[v], currentVertex)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean visit(List<List<Integer>> adjacencyList, boolean[] visited, boolean[] checkCycle, int currentVertex){
-        if(!visited[currentVertex]){
-            if(checkCycle[currentVertex]){
-                return false;
-            }
-            checkCycle[currentVertex] = true;
-            for(int nextNode : adjacencyList.get(currentVertex)){
-                if(!visit(adjacencyList, visited, checkCycle, nextNode)){
-                    return false;
-                }
+        checkCycle[currentVertex] = true;
+        for(int nextNode : adjacencyList.get(currentVertex)){
+            if(dfs(adjacencyList, visited, checkCycle, nextNode, result)) {
+                return true;
             }
         }
         checkCycle[currentVertex] = false;
         visited[currentVertex] = true;
-        return true;
+        result.push(currentVertex);
+        return false;
     }
 
     public static List<List<Integer>> buildGraph(int[][] graph, int v){
@@ -72,19 +52,9 @@ public class TopologicalSort_DFS {
     }
 
     public static void main(String[] args) {
-        int[][] graph = new int[][]{ {5, 2}, {5, 0}, {4,0}, {4, 1}, {2, 3}, {3, 1} };
-        Stack<Integer> temp = topologicalSort(graph, 6);
-        while(!temp.isEmpty()){
-            System.out.println(temp.pop());
-        }
-        int[][] graph2 = new int[][]{ {5, 2}, {5, 0}, {4,0}, {4, 1}, {2, 3}, {3, 1} };
-        boolean tf = topologicalSortDetectCycles(graph2, 6);
-        if(!tf){
-            System.out.println("Cycle detected");
-        }
-        else{
-            System.out.println("No cycle detected");
-        }
-
+        int[][] graphWithCycle = new int[][]{ {5, 2}, {5, 0}, {4,0}, {4, 1}, {2, 3}, {3, 1}, {2, 5} };
+        int[][] graphWithoutCycle = new int[][]{ {5, 2}, {5, 0}, {4,0}, {4, 1}, {2, 3}, {3, 1} };
+        Stack<Integer> temp = topologicalSort(graphWithoutCycle, 6);
+        Stack<Integer> temp2 = topologicalSort(graphWithCycle, 6);
     }
 }
