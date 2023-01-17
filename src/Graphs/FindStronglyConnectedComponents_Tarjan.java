ddfde5c43b8f0b14c0bfbdbcb1c8b392e3c77da9
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-16 18:27:59 -0800

    WIP

--- src/Graphs/FindStronglyConnectedComponents_Tarjan.java
+++ src/Graphs/FindStronglyConnectedComponents_Tarjan.java
@@ -1,54 +1,55 @@
 package Graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class FindStronglyConnectedComponents_Tarjan {
-    static int count = 0;
-    public static int findStronglyConnectedComponents(int[][] graph, int v){
+    public static List<List<Integer>> findStronglyConnectedComponents(int[][] graph, int v){
         List<List<Integer>> adjacencyList = buildGraph(graph, v);
+        List<List<Integer>> components = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
         boolean[] onStack = new boolean[v];
-        int[] ids = new int[v];
+        int[] indices = new int[v];
         int[] low = new int[v];
-        int id = 0;
+        int index = 0;
 
+        Arrays.fill(indices, -1);
         for(int x = 0; x < v; x++){
-            ids[x] = -1;
-        }
-        for(int x = 0; x < v; x++){
-            if(ids[x] == -1){
-                dfs(adjacencyList, stack, onStack, ids, low, x, id);
+            if(indices[x] == -1){
+                dfs(adjacencyList, components, stack, onStack, indices, low, x, index);
             }
         }
-        return count;
+        return components;
     }
 
-    public static void dfs(List<List<Integer>> adjacencyList, Stack<Integer> stack, boolean[] onStack, int[] ids, int[] low, int currentVertex, int id){
+    public static void dfs(List<List<Integer>> adjacencyList, List<List<Integer>> components, Stack<Integer> stack, boolean[] onStack, int[] indices, int[] low, int currentVertex, int index){
+        indices[currentVertex] = low[currentVertex] = index++;
         stack.push(currentVertex);
         onStack[currentVertex] = true;
-        ids[currentVertex] = id;
-        low[currentVertex] = id;
-        id++;
-        for(int nextVertex : adjacencyList.get(currentVertex)){
-            if(ids[nextVertex] == -1){
-                dfs(adjacencyList, stack, onStack, ids, low, nextVertex, id);
+
+        for(int nextVertex : adjacencyList.get(currentVertex)) {
+            if(indices[nextVertex] == -1){
+                dfs(adjacencyList, components, stack, onStack, indices, low, nextVertex, index);
             }
             if(onStack[nextVertex]){
                 low[currentVertex] = Math.min(low[currentVertex], low[nextVertex]);
             }
         }
-        if(ids[currentVertex] == low[currentVertex]){
+
+        if(indices[currentVertex] == low[currentVertex]) {
+            List<Integer> temp = new ArrayList<>();
             while(!stack.isEmpty()){
                 int x = stack.pop();
+                temp.add(x);
                 onStack[x] = false;
-                low[x] = ids[currentVertex];
+                low[x] = indices[currentVertex];
                 if(x == currentVertex){
                     break;
                 }
             }
-            count++;
+            components.add(temp);
         }
     }
 
@@ -65,6 +66,15 @@ public class FindStronglyConnectedComponents_Tarjan {
 
     public static void main(String[] args) {
         int[][] graph = { {0,1}, {1,2}, {1,4}, {1,6}, {2,3}, {3,2}, {3,5}, {3,4}, {4,5}, {5,4}, {6,0}, {6,2} };
-        System.out.println(findStronglyConnectedComponents(graph, 7));
+        List<List<Integer>> components = findStronglyConnectedComponents(graph, 7);
+        System.out.println("Number of components: " + components.size());
+        int count = 1;
+        for(List<Integer> temp : components){
+            System.out.print("Component " + count++ + ": ");
+            for(int x : temp){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
     }
 }
