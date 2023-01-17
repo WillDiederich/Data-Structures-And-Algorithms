ad2146f6956e438d0151404e24f52486f2bede58
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-16 18:41:50 -0800

    ...

--- src/Graphs/FindArticulationPoints_Tarjan.java
+++ src/Graphs/FindArticulationPoints_Tarjan.java
@@ -1,5 +1,72 @@
 package Graphs;
 
-public class FindArticulationPoints_Tarjan {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FindArticulationPoints_Tarjan {
+    static int time = 0;
+    public static List<Integer> findBridges(int[][] graph, int v){
+        List<List<Integer>> adjacencyList = buildGraph(graph, v);
+        List<Integer> articulationPoints = new ArrayList<>();
+        // Discovered contains the time that each vertex was discovered.
+        int[] discovered = new int[v];
+        // Low contains the lowest vertex that each vertex can reach.
+        // Good explanation: https://youtu.be/wUgWX0nc4NY?t=70
+        int[] low = new int[v];
+        for(int x = 0; x < v; x++){
+            discovered[x] = -1;
+        }
+        for(int x = 0; x < v; x++){
+            if(discovered[x] == -1){
+                dfs(adjacencyList, articulationPoints, discovered, low, x, x);
+            }
+        }
+        return articulationPoints;
+    }
+
+    public static void dfs(List<List<Integer>> adjacencyList, List<Integer> articulationPoints, int[] discovered, int[] low, int current, int parent){
+        int children = 0;
+        time++;
+        discovered[current] = time;
+        low[current] = time;
+        for(int nextVertex : adjacencyList.get(current)){
+            if(nextVertex != parent){
+                if(discovered[nextVertex] == -1) {
+                    dfs(adjacencyList, articulationPoints, discovered, low, nextVertex, current);
+                    low[current] = Math.min(low[current], low[nextVertex]);
+                    children++;
+                    if(low[nextVertex] >= discovered[current] && parent != current){
+                        articulationPoints.add(current);
+                    }
+                }
+                else if(nextVertex != parent){
+                    low[current] = Math.min(low[current], discovered[nextVertex]);
+                }
+            }
+        }
+        if(parent == current && children > 1){
+            articulationPoints.add(current);
+        }
+    }
+
+    public static List<List<Integer>> buildGraph(int[][] graph, int v){
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        for (int x = 0; x < v; x++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        for (int[] edge : graph) {
+            adjacencyList.get(edge[0]).add(edge[1]);
+            // Include the below line when creating an undirected graph
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+        return adjacencyList;
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = { {0,1}, {1,2}, {2,0}, {1,3} };
+        List<Integer> result = findBridges(graph, 4);
+        for(int x : result){
+            System.out.println(x);
+        }
+    }
 }
