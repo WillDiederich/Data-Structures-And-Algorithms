8394ad5ebb4a53b2ac1e07d23bf77cee70e07750
WillDiederich <59210947+WillDiederich@users.noreply.github.com>
2023-01-18 15:34:26 -0800

    ...

--- src/Graphs/FindArticulationPoints_Tarjan.java
+++ src/Graphs/FindArticulationPoints_Tarjan.java
@@ -1,6 +1,7 @@
 package Graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindArticulationPoints_Tarjan {
@@ -10,43 +11,41 @@ public class FindArticulationPoints_Tarjan {
         List<Integer> articulationPoints = new ArrayList<>();
         // Discovered contains the time that each vertex was discovered.
         int[] discovered = new int[v];
-        // Low contains the lowest vertex that each vertex can reach.
-        int[] low = new int[v];
-        // -1 denotes that a node hasn't been visited.
-        for(int x = 0; x < v; x++){
-            discovered[x] = -1;
-        }
+        // The low-link value of a vertex is the lowest node that can be reached from that node during a depth-first search
+        int[] lowLink = new int[v];
+
+        Arrays.fill(discovered, -1);
         // Perform dfs on all unvisited nodes.
         for(int x = 0; x < v; x++){
             if(discovered[x] == -1){
-                dfs(adjacencyList, articulationPoints, discovered, low, x, x);
+                dfs(adjacencyList, articulationPoints, discovered, lowLink, x, x);
             }
         }
         return articulationPoints;
     }
 
-    public static void dfs(List<List<Integer>> adjacencyList, List<Integer> articulationPoints, int[] discovered, int[] low, int current, int parent){
+    public static void dfs(List<List<Integer>> adjacencyList, List<Integer> articulationPoints,
+                           int[] discovered, int[] lowLink, int currentVertex, int parent){
         int children = 0;
         time++;
-        discovered[current] = time;
-        low[current] = time;
-        for(int nextVertex : adjacencyList.get(current)){
-            if(nextVertex != parent){
-                if(discovered[nextVertex] == -1) {
-                    dfs(adjacencyList, articulationPoints, discovered, low, nextVertex, current);
-                    low[current] = Math.min(low[current], low[nextVertex]);
-                    children++;
-                    if(low[nextVertex] >= discovered[current] && parent != current){
-                        articulationPoints.add(current);
-                    }
+        discovered[currentVertex] = time;
+        lowLink[currentVertex] = time;
+        // Visit every neighbor of the current vertex
+        for(int nextVertex : adjacencyList.get(currentVertex)){
+            if(nextVertex != parent && discovered[nextVertex] == -1){
+                dfs(adjacencyList, articulationPoints, discovered, lowLink, nextVertex, currentVertex);
+                lowLink[currentVertex] = Math.min(lowLink[currentVertex], lowLink[nextVertex]);
+                children++;
+                if(lowLink[nextVertex] >= discovered[currentVertex] && parent != currentVertex){
+                    articulationPoints.add(currentVertex);
                 }
                 else if(nextVertex != parent){
-                    low[current] = Math.min(low[current], discovered[nextVertex]);
+                    lowLink[currentVertex] = Math.min(lowLink[currentVertex], discovered[nextVertex]);
                 }
             }
         }
-        if(parent == current && children > 1){
-            articulationPoints.add(current);
+        if(parent == currentVertex && children > 1){
+            articulationPoints.add(currentVertex);
         }
     }
 
--- src/Graphs/FindStronglyConnectedComponents_Tarjan.java
+++ src/Graphs/FindStronglyConnectedComponents_Tarjan.java
@@ -11,7 +11,9 @@ public class FindStronglyConnectedComponents_Tarjan {
         List<List<Integer>> components = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
         boolean[] onStack = new boolean[v];
+        // The incides array serves to track the index value of a node, as well as if a node has been visited
         int[] indices = new int[v];
+        // The low-link value of a vertex is the lowest node that can be reached from that node during a depth-first search
         int[] low = new int[v];
         int index = 0;
 
@@ -29,22 +31,32 @@ public class FindStronglyConnectedComponents_Tarjan {
         indices[currentVertex] = low[currentVertex] = index++;
         stack.push(currentVertex);
         onStack[currentVertex] = true;
+        // Visit every neighbor of the current vertex.
         for(int nextVertex : adjacencyList.get(currentVertex)) {
+            // If the next vertex has not been visited, perform dfs on it.
             if(indices[nextVertex] == -1) {
                 dfs(adjacencyList, components, stack, onStack, indices, low, nextVertex, index);
             }
+            // If the next vertex is on the stack, set the low link value of the current vertex to the min low link between
+            // the current vertex and the next vertex
             if(onStack[nextVertex]) {
                 low[currentVertex] = Math.min(low[currentVertex], low[nextVertex]);
             }
         }
 
+        // Check if we're at the start of a strongly connected component
+        // The start of a strongly connected component is indicated by the index of the current vertex equaling its low link value
         if(indices[currentVertex] == low[currentVertex]) {
             List<Integer> temp = new ArrayList<>();
+            // Pop every node contained within the strongly connected component from the stack
             while(!stack.isEmpty()) {
                 int x = stack.pop();
                 temp.add(x);
+                // Set the onStack value to false
                 onStack[x] = false;
+                // Make every low link value for all nodes in the scc the same.
                 low[x] = indices[currentVertex];
+                // break when we've found the start of the scc.
                 if(x == currentVertex) {
                     break;
                 }
@@ -67,7 +79,7 @@ public class FindStronglyConnectedComponents_Tarjan {
     public static void main(String[] args) {
         int[][] graph = { {0,1}, {1,2}, {1,4}, {1,6}, {2,3}, {3,2}, {3,5}, {3,4}, {4,5}, {5,4}, {6,0}, {6,2} };
         List<List<Integer>> components = findStronglyConnectedComponents(graph, 7);
-        System.out.println("Number of components: " + components.size());
+        System.out.println("Number of strongly connected components: " + components.size());
         int count = 1;
         for(List<Integer> temp : components) {
             System.out.print("Component " + count++ + ": ");
